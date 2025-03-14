189bebeaae1f01a3985e232ff4bf89ff639bd957
GitHub Action <action@github.com>
2025-03-14 12:03:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/nyub/selfieintellijplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/nyub/selfieintellijplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nyub.selfieintellijplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/nyub/selfieintellijplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/nyub/selfieintellijplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.nyub.selfieintellijplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.nyub.selfieintellijplugin.MyBundle
+import com.github.nyub.selfieintellijplugin.services.MyProjectService
 import javax.swing.JButton
 
 
