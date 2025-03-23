6fc25c6e348d972d5e394d1feb4c8d397df582e2
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-03-23 12:22:57 +0100

    Keep mixin class name coherent with mixed-in class

--- src/main/kotlin/io/github/nyub/selfieintellijplugin/language/psi/SelfieBodyMixin.kt
+++ src/main/kotlin/io/github/nyub/selfieintellijplugin/language/psi/SelfieBodyMixin.kt
@@ -7,7 +7,7 @@ import com.intellij.psi.ElementManipulators
 import com.intellij.psi.LiteralTextEscaper
 import com.intellij.psi.PsiLanguageInjectionHost
 
-open class SelfieSnapshotBodyMixin(node: ASTNode) : ASTWrapperPsiElement(node), SelfieBody {
+open class SelfieBodyMixin(node: ASTNode) : ASTWrapperPsiElement(node), SelfieBody {
     override fun isValidHost(): Boolean = true
 
     override fun updateText(text: String): PsiLanguageInjectionHost {
@@ -15,7 +15,7 @@ open class SelfieSnapshotBodyMixin(node: ASTNode) : ASTWrapperPsiElement(node), 
     }
 
     override fun createLiteralTextEscaper(): LiteralTextEscaper<out PsiLanguageInjectionHost> {
-        return object : LiteralTextEscaper<SelfieSnapshotBodyMixin>(this) {
+        return object : LiteralTextEscaper<SelfieBodyMixin>(this) {
             override fun decode(rangeInsideHost: TextRange, outChars: StringBuilder): Boolean {
                 outChars.append(text)
                 return true
