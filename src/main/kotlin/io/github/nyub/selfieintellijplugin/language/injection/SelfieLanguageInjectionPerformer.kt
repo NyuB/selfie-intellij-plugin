5b38067f5b372a6e86bc7364fdbbcfc96cbd9fe5
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-03-24 06:22:20 +0100

    Use with construct for repeated delegated calls

--- src/main/kotlin/io/github/nyub/selfieintellijplugin/language/injection/SelfieLanguageInjectionPerformer.kt
+++ src/main/kotlin/io/github/nyub/selfieintellijplugin/language/injection/SelfieLanguageInjectionPerformer.kt
@@ -8,16 +8,18 @@ import com.intellij.psi.PsiElement
 import io.github.nyub.selfieintellijplugin.language.psi.SelfieBody
 
 class SelfieLanguageInjectionPerformer : LanguageInjectionPerformer {
-    override fun isPrimary(): Boolean {
-        return true
-    }
+    override fun isPrimary(): Boolean = true
 
     override fun performInjection(registrar: MultiHostRegistrar, injection: Injection, context: PsiElement): Boolean {
         val host = context as? SelfieBody ?: return false
         val language = injection.injectedLanguage ?: return false
-        registrar.startInjecting(language)
-        registrar.addPlace(null, null, host, TextRange(0, host.textLength))
-        registrar.doneInjecting()
+
+        with(registrar) {
+            startInjecting(language)
+            addPlace(null, null, host, TextRange(0, host.textLength))
+            doneInjecting()
+        }
+
         return true
     }
 }
